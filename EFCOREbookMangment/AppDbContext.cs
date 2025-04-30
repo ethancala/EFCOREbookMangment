f253f61aa0918aa4b62aff33c5136e379e00c38b
Ethan <ecala420@gmail.com>
2025-04-30 13:18:53 -0500

    added DB functionality. Current working branch

--- EFCOREbookMangment/AppDbContext.cs
+++ EFCOREbookMangment/AppDbContext.cs
@@ -7,7 +7,7 @@ public class AppDbContext : DbContext
 {
     
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer(@"Server=Ethans-PC;Database=LibraryDB;Trusted_Connection=True");
+        => options.UseSqlServer(@"Server=Ethans-PC;Database=LibraryDB;Trusted_Connection=True; TrustServerCertificate=True");
     
     public DbSet<Author> Authors { get; set; }
     public DbSet<Book> Books { get; set; }
