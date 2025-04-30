d0253752667259cec456088b22e43591b8c672bd
Ethan <ecala420@gmail.com>
2025-04-30 13:11:19 -0500

    Added files

---
+++ EFCOREbookMangment/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace EFCOREbookMangment;
+
+using Microsoft.EntityFrameworkCore;  // Essential for DbContext
+
+
+public class AppDbContext : DbContext
+{
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(@"Server=Ethans-PC;Database=LibraryDB;Trusted_Connection=True");
+    
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+
+}
--- EFCOREbookMangment/Program.cs
+++ EFCOREbookMangment/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace EFCOREbookMangment;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-class Program
+namespace EFCOREbookMangment
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static async Task Main(string[] args)
+        {
+            using var context = new AppDbContext();
+            context.Database.EnsureCreated();
+
+            if (!await context.Authors.AnyAsync())
+            {
+                var authors = new List<Author>
+                {
+                    new() { FirstName = "J.K.", LastName = "Rowling", Books = new()
+                    {
+                        new() { Title = "Harry Potter 1", PublicationYear = 1997 },
+                        new() { Title = "Harry Potter 2", PublicationYear = 1998 }
+                    }},
+                    new() { FirstName = "George", LastName = "Orwell", Books = new()
+                    {
+                        new() { Title = "1984", PublicationYear = 1949 }
+                    }}
+                };
+
+                await context.Authors.AddRangeAsync(authors);
+                await context.SaveChangesAsync();
+            }
+        }
     }
 }
